6bc1600834d774cab1b2ec2473e5401072cdaf63
Alan Aguiar <alanjas@hotmail.com>
2012-01-21 00:30:28 -0200

    version 4

--- activity.py
+++ activity.py
@@ -2,7 +2,8 @@
 # -*- coding: utf-8 -*-
 #
 # Pointillism
-# Copyright (C) 2011, 2012
+# Copyright (C) 2008, Nirav Patel
+# Copyright (C) 2011, 2012, Alan Aguiar
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -19,44 +20,109 @@
 #
 # Contact information:
 # Alan Aguiar <alanjas@gmail.com>
-#
+# Nirav Patel <sugarlabs@spongezone.net>
 
 import os
 import sys
+import gtk
 sys.path.insert(0, "lib")
 import pygame
-import olpcgames
-from olpcgames import activity
+import sugargame
+import sugargame.canvas
+from sugar.activity import activity
+from sugar.graphics.toolbutton import ToolButton
 from sugar.datastore import datastore
 from sugar.graphics.toolbutton import ToolButton
 from gettext import gettext as _
+import puntillism
 
-class Activity(activity.PyGameActivity):
-    
-    game_name = 'run'
-    game_title = _('Pointillism')
-    game_size = (1200, 900)
+class Activity(activity.Activity):
+
+    def __init__(self, handle):
+        activity.Activity.__init__(self, handle)
+
+        self.radio_uno = 2
+        self.radio_dos = 12
+
+        self.actividad = puntillism.Puntillism(self)
+
+        self.build_toolbar()
+        self._pygamecanvas = sugargame.canvas.PygameCanvas(self)
+        self.set_canvas(self._pygamecanvas)
+        self._pygamecanvas.run_pygame(self.actividad.run)
 
     def build_toolbar(self):
-        toolbar = super( Activity, self).build_toolbar()
-        toolbar.keep.props.visible = False
+
+        caja = activity.ActivityToolbox(self)
+
+        toolbar = caja.get_activity_toolbar()
+        toolbar.remove(toolbar.keep)
+        toolbar.keep = None
+        toolbar.remove(toolbar.share)
+        toolbar.share = None
+
+        separador1 = gtk.SeparatorToolItem()
+        separador1.props.draw = True
+        toolbar.insert(separador1, 1)
+
+        item1 = gtk.ToolItem()
+        self.label_radio1 = gtk.Label()
+        self.label_radio1.set_text(_('Circles between '))
+        item1.add(self.label_radio1)
+        toolbar.insert(item1, 2)
+
+        item2 = gtk.ToolItem()
+        self.cradio1 = gtk.SpinButton()
+        self.cradio1.set_range(1, 20)
+        self.cradio1.set_increments(1, 2)
+        self.cradio1.props.value = self.radio_uno
+        self.cradio1_handler = self.cradio1.connect('notify::value', self.cradio1_valor)
+        item2.add(self.cradio1)
+        toolbar.insert(item2, 3)
+
+        item3 = gtk.ToolItem()
+        self.label_and = gtk.Label()
+        self.label_and.set_text(_(' and '))
+        item3.add(self.label_and)
+        toolbar.insert(item3, 4)
+
+        item4 = gtk.ToolItem()
+        self.cradio2 = gtk.SpinButton()
+        self.cradio2.set_range(1, 20)
+        self.cradio2.set_increments(1, 2)
+        self.cradio2.props.value = self.radio_dos
+        self.cradio2_handler = self.cradio2.connect('notify::value', self.cradio2_valor)
+        item4.add(self.cradio2)
+        toolbar.insert(item4, 5)
+
         savebutton = ToolButton('filesave')
         savebutton.set_tooltip(_('Save Image'))
         savebutton.connect('clicked',self._savebutton_cb)
-        toolbar.insert(savebutton, 2)
-        savebutton.show()
-        
-        return toolbar
+        toolbar.insert(savebutton, 7)
+        #savebutton.show()
+
+        caja.show_all()
+        self.set_toolbox(caja)
+
+    def cradio1_valor(self, radio, value):
+        self.radio_uno = int(radio.props.value)
+        self.actividad.poner_radio1(self.radio_uno)
+
+    def cradio2_valor(self, radio, value):
+        self.radio_dos = int(radio.props.value)
+        self.actividad.poner_radio2(self.radio_dos)
 
     def _savebutton_cb(self,button):
-        pygame.event.post(olpcgames.eventwrap.Event(pygame.USEREVENT, action='savebutton'))
+        pygame.event.post(pygame.event.Event(pygame.USEREVENT, action='savebutton'))
 
     def save_image(self,image):
         journalobj = datastore.create()
         journalobj.metadata['title'] = 'Pointillism'
         journalobj.metadata['mime_type'] = 'image/jpeg'
-        
-        file_path = os.path.join(olpcgames.ACTIVITY.get_activity_root(),'instance','pointillism.jpg')
+
+        #file_path = os.path.join(olpcgames.ACTIVITY.get_activity_root(),'instance','pointillism.jpg')
+
+        file_path = os.path.join(os.environ['SUGAR_ACTIVITY_ROOT'], 'data', 'pointillism.jpg')
 
         pygame.image.save(image,file_path)
         journalobj.set_file_path(file_path)
---
+++ puntillism.py
@@ -0,0 +1,113 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Pointillism
+# Copyright (C) 2008, Nirav Patel
+# Copyright (C) 2011, 2012, Alan Aguiar
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Contact information:
+# Alan Aguiar <alanjas@gmail.com>
+# Nirav Patel <sugarlabs@spongezone.net>
+
+import os
+import sys
+import gtk
+sys.path.insert(0, "lib")
+import random
+import pygame
+from pygame import camera
+
+#import logging
+#log = logging.getLogger( 'Pointillism run' )
+#log.setLevel( logging.ERROR )
+
+class Puntillism():
+
+    def __init__(self, parent):
+        self.parent = parent
+        #logging.basicConfig()
+
+    def poner_radio1(self, radio):
+        self.radio1 = radio
+
+    def poner_radio2(self, radio):
+        self.radio2 = radio
+
+    def run(self):
+        #size = (1200,900)
+        pygame.init()
+        pygame.camera.init()
+        #camera.init()
+
+        self.radio1 = 2
+        self.radio2 = 12
+
+        screen = pygame.display.get_surface()
+        screen.fill((0,0,0))
+        pygame.display.flip()
+
+        clock = pygame.time.Clock()
+
+        cam = camera.Camera("/dev/video0", (640, 480), "RGB")
+        cam.set_controls(True, False)
+        cam.start()
+
+        cap = pygame.surface.Surface((640, 480), 0, screen)
+        frames = 0
+        running = True
+        while running:
+            cap = cam.get_image(cap)
+            rect = []
+            for z in range(max(20, int(frames)*10)):
+                x = random.random()
+                y = random.random()
+                if self.radio1 > self.radio2:
+                    aux = self.radio2
+                    self.radio2 = self.radio1
+                    self.radio1 = aux
+                elif self.radio1 == self.radio2:
+                    self.radio2 = self.radio2 + 1
+                num = random.randrange(self.radio1, self.radio2, 1)
+                rect.append(pygame.draw.circle(screen, cap.get_at((int(x * 640), int(y * 480))), (int(x * 1200), int(y * 900)), num, 0))
+            pygame.display.update(rect)
+
+            clock.tick()
+            frames = clock.get_fps()
+
+            #GTK events
+            while gtk.events_pending():
+                gtk.main_iteration()
+
+            events = pygame.event.get()
+            for event in events:
+                #log.debug( "Event: %s", event )
+                if event.type == pygame.QUIT:
+                    cam.stop()
+                    running = False
+                elif event.type == pygame.VIDEORESIZE:
+                    pygame.display.set_mode(event.size, pygame.RESIZABLE)
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        cam.stop()
+                        running = False
+                    elif event.key == pygame.K_s:
+                        self.parent.save_image(screen)
+                elif event.type == pygame.USEREVENT:
+                    if hasattr(event,'action'):
+                        if event.action == 'savebutton':
+                            self.parent.save_image(screen)
+            #pygame.display.flip()
+
